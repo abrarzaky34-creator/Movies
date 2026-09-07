91bf5b34eea7c5ad0618cde99877458ef858d3ec
malak <mallakwakeel@gmail.com>
2026-09-08 01:59:56 +0300

    add movie suggestions API

---
+++ lib/features/suggestions/presentation/cubit/suggestions_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/data_source/movie_suggestion_data_source.dart';
+import '../../data/models/movie_suggestions_model.dart';
+import 'suggestions_state.dart';
+
+class SuggestionsCubit extends Cubit<SuggestionsState> {
+  final MovieSuggestionsDataSource dataSource;
+
+  SuggestionsCubit(this.dataSource)
+      : super(SuggestionsInitial());
+
+  Future<void> getSuggestions(int movieId) async {
+    emit(SuggestionsLoading());
+
+    try {
+      final movies =
+      await dataSource.getMovieSuggestions(movieId);
+
+      emit(SuggestionsSuccess(movies));
+    } catch (e) {
+      emit(SuggestionsError(e.toString()));
+    }
+  }
+}
