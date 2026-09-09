90d7b1bf1995530544b87a1420fdc0a7749d1735
Abrar Zaki <abrarzaky34@gmail.com>
2026-09-09 03:03:43 +0300

    chore: unify feature folders into features/ and fix imports

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import '../../feature/auth/presentation/screens/splash_screen.dart';
-import '../../feature/auth/presentation/screens/onboarding_screen_1.dart';
-import '../../feature/auth/presentation/screens/login_screen.dart';
-import '../../feature/auth/presentation/screens/register_screen.dart';
-import '../../feature/auth/presentation/screens/forget_password_screen.dart';
-import '../../feature/auth/presentation/screens/update_profile_screen.dart';
-import '../../feature/auth/presentation/screens/profile_tab.dart';
+import '../../features/auth/presentation/screens/splash_screen.dart';
+import '../../features/auth/presentation/screens/onboarding_screen_1.dart';
+import '../../features/auth/presentation/screens/login_screen.dart';
+import '../../features/auth/presentation/screens/register_screen.dart';
+import '../../features/auth/presentation/screens/forget_password_screen.dart';
+import '../../features/auth/presentation/screens/update_profile_screen.dart';
+import '../../features/auth/presentation/screens/profile_tab.dart';
 import '../../features/home/presentation/pages/main_layout_screen.dart';
-import '../../feature/movie_details/presentation/screens/movie_details_screen.dart';
+import '../../features/movie_details/presentation/screens/movie_details_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
