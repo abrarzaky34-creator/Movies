6e90406c95d1b1decced5b14b8738ba0d7e132f1
Abrar Zaki <abrarzaky34@gmail.com>
2026-09-09 23:12:46 +0300

    Accept alaaa auth module

---
+++ lib/languages/app_localizations_ar.dart
@@ -0,0 +1,46 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get register => 'إنشاء حساب';
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get profile => 'الملف الشخصي';
+
+  @override
+  String get search => 'بحث';
+
+  @override
+  String get watchList => 'قائمة المشاهدة';
+
+  @override
+  String get history => 'السجل';
+
+  @override
+  String get editProfile => 'تعديل الملف الشخصي';
+
+  @override
+  String get exit => 'خروج';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get english => 'الإنجليزية';
+
+  @override
+  String get arabic => 'العربية';
+}
